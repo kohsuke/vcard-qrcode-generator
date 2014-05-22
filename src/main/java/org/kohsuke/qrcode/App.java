22f296f480e76f8e67bf19f99fe1ecfd9b4748d8
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-21 22:52:00 -0700

    Sort by name

--- src/main/java/org/kohsuke/qrcode/App.java
+++ src/main/java/org/kohsuke/qrcode/App.java
@@ -1,6 +1,5 @@
 package org.kohsuke.qrcode;
 
-import com.google.zxing.WriterException;
 import com.googlecode.jcsv.CSVStrategy;
 import com.googlecode.jcsv.reader.CSVReader;
 import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
@@ -8,27 +7,54 @@ import com.googlecode.jcsv.reader.internal.DefaultCSVEntryParser;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws WriterException, IOException {
+
+    private List<String> headers;
+
+    public static void main(String[] args) throws Exception {
+        new App().run();
+    }
+
+    public void run() throws Exception {
         CSVReader<String[]> csv = new CSVReaderBuilder<String[]>(new FileReader("input.csv"))
                 .strategy(CSVStrategy.UK_DEFAULT)
                 .entryParser(new DefaultCSVEntryParser())
                 .build();
 
-        List<String> headers = csv.readHeader();
+        headers = csv.readHeader();
 
         HtmlWriter o = new HtmlWriter(new File("."));
-        for (String[] row : csv.readAll()) {
+        List<String[]> rows = csv.readAll();
+        Collections.sort(rows, comparator);
+
+        for (String[] row : rows) {
             o.add(
-                    row[headers.indexOf("First Name")],
-                    row[headers.indexOf("Last Name")],
-                    row[headers.indexOf("Company")],
-                    row[headers.indexOf("Email")],
-                    row[headers.indexOf("Work Phone")]);
+                    row[column("First Name")],
+                    row[column("Last Name")],
+                    row[column("Company")],
+                    row[column("Email")],
+                    row[column("Work Phone")]);
         }
         o.close();
     }
+
+    private int column(String name) {
+        return headers.indexOf(name);
+    }
+
+    private Comparator<String[]> comparator = new Comparator<String[]>() {
+        public int compare(String[] a, String[] b) {
+            int r = cmp(a, b, column("First Name"));
+            if (r!=0)   return r;
+            return cmp(a, b, column("Last Name"));
+        }
+
+        public int cmp(String[] a, String[] b, int column) {
+            return a[column].compareTo(b[column]);
+        }
+    };
 }
